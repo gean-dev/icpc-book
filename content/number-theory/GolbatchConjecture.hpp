1e9cae6a00a33d69c12ddda4f67bdbcc4f9828f8
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-01-06 01:12:04 +0700

    feat: move golbatch conjecture

---
+++ content/number-theory/GolbatchConjecture.hpp
@@ -0,0 +1,29 @@
+/**
+ * Author: Borworntat D.
+ * Date: 2023-12-02
+ * License: CC0
+ * Source: https://www.geeksforgeeks.org/program-for-goldbachs-conjecture-two-primes-with-given-sum/
+ * Description: Find two prime numbers which sum equals $s$
+ * Time: $O(N\log{N})$
+ */
+
+#include "FastEratosthenes.hpp"
+
+pair<int, int> goldbatchConjecture(int s, vi pr = {}){
+  if (s <= 2 || s % 2 != 0) {
+    return make_pair(-1, -1);
+  }
+  if (pr.size() == 0) {
+    pr = eratosthenes();
+  }
+  for (auto x : pr) {
+    if (x > s / 2) {
+      break;
+    }
+    int d = s - x;
+    if (binary_search(pr.begin(), pr.end(), d)) {
+      return make_pair(min(x, d), max(x, d));
+    }
+  }
+  return make_pair(-1, -1);
+}
