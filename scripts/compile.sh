0794396cbb0fcb538bb06ae184adeae61d9923b9
ttamx <teetat.tata@gmail.com>
2024-05-19 02:36:10 +0700

    fix: change compiler from g++-13 to g++

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -4,4 +4,4 @@ echo "
 #include \"../$FILE\"
 int main() {}
 " >build/temp.cpp
-g++-13 -Wall -Wextra -Wfatal-errors -Wconversion -std=c++2a build/temp.cpp && rm a.out build/temp.cpp
+g++- -Wall -Wextra -Wfatal-errors -Wconversion -std=c++2a build/temp.cpp && rm a.out build/temp.cpp
