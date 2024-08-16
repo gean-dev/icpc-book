80a6a060ce4b34908feab956b5fd1ab5e0ccb718
ttamx <teetat.tata@gmail.com>
2024-08-16 23:54:41 +0700

    feat: flow

--- content/graph/Dinic.hpp
+++ content/graph/Dinic.hpp
@@ -1,79 +1,97 @@
 #pragma once
-#include "../template/Header.hpp"
 
 /**
  * Author: Teetat T.
- * Date: 2024-03-31
- * Description: Fast max-flow algorithm.
- * Time: $O(VE\log U)$ where $U = \max |\text{cap}|$
+ * Date: 2024-07-15
+ * Description: Dinic's Algorithm for finding the maximum flow.
+ * Time: O(V E \log U) where U is the maximum flow.
  */
 
-template<class T>
+template<class T,bool directed=true,bool scaling=true>
 struct Dinic{
+    static constexpr T INF=numeric_limits<T>::max()/2;
     struct Edge{
         int to;
-        ll flow,cap;
-        Edge(int _to,ll _cap):to(_to),flow(0),cap(_cap){}
-        ll getcap(){
-            return cap-flow;
-        }
+        T flow,cap;
+        Edge(int _to,T _cap):to(_to),flow(0),cap(_cap){}
+        T remain(){return cap-flow;}
     };
-    int n;
-    ll U;
+    int n,s,t;
+    T U;
     vector<Edge> e;
-    vector<vi> adj;
-    vi ptr,lvl;
+    vector<vector<int>> g;
+    vector<int> ptr,lv;
+    bool calculated;
+    T max_flow;
     Dinic(){}
-    Dinic(int _n){
-        init(_n);
-    }
-    void init(int _n){
-        n=_n,U=0;
+    Dinic(int n,int s,int t){init(n,s,t);}
+    void init(int _n,int _s,int _t){
+        n=_n,s=_s,t=_t;
+        U=0;
         e.clear();
-        adj.assign(n,{});
+        g.assign(n,{});
+        calculated=false;
     }
-    void addEdge(int u,int v,ll cap){
+    void add_edge(int from,int to,T cap){
+        assert(0<=from&&from<n&&0<=to&&to<n);
+        g[from].emplace_back(e.size());
+        e.emplace_back(to,cap);
+        g[to].emplace_back(e.size());
+        e.emplace_back(from,directed?0:cap);
         U=max(U,cap);
-        adj[u].emplace_back(sz(e));
-        e.emplace_back(v,cap);
-        adj[v].emplace_back(sz(e));
-        e.emplace_back(u,0); // change 0 to cap for undirected flow
     }
-    bool bfs(int s,int t,ll scale){
-        lvl.assign(n,0);
-        vi q{s};
-        lvl[s]=1;
-        for(int i=0;i<sz(q);i++){
+    bool bfs(T scale){
+        lv.assign(n,-1);
+        vector<int> q{s};
+        lv[s]=0;
+        for(int i=0;i<(int)q.size();i++){
             int u=q[i];
-            for(auto j:adj[u])if(!lvl[e[j].to]&&e[j].getcap()>=scale){
-                q.emplace_back(e[j].to);
-                lvl[e[j].to]=lvl[u]+1;
+            for(int j:g[u]){
+                int v=e[j].to;
+                if(lv[v]==-1&&e[j].remain()>=scale){
+                    q.emplace_back(v);
+                    lv[v]=lv[u]+1;
+                }
             }
         }
-        return lvl[t];
+        return lv[t]!=-1;
     }
-    ll dfs(int u,int t,ll f){
-        if(u==t||!f)return f;
-        for(int &i=ptr[u];i<sz(adj[u]);i++){
-            int j=adj[u][i];
-            if(lvl[e[j].to]==lvl[u]+1){
-                if(ll p=dfs(e[j].to,t,min(f,e[j].getcap()))){
-                    e[j].flow+=p;
-                    e[j^1].flow-=p;
-                    return p;
+    T dfs(int u,int t,T f){
+        if(u==t||f==0)return f;
+        for(int &i=ptr[u];i<(int)g[u].size();i++){
+            int j=g[u][i];
+            int v=e[j].to;
+            if(lv[v]==lv[u]+1){
+                T res=dfs(v,t,min(f,e[j].remain()));
+                if(res>0){
+                    e[j].flow+=res;
+                    e[j^1].flow-=res;
+                    return res;
                 }
             }
         }
         return 0;
     }
-    ll flow(int s,int t){
-        ll flow=0;
-        for(ll L=1ll<<(63-__builtin_clzll(U));L>0;L>>=1) // L = 1 may be faster but it's O(V^2 E)
-        while(bfs(s,t,L)){
-            ptr.assign(n,0);
-            while(ll p=dfs(s,t,LINF))flow+=p;
-        };
-        return flow;
+    T flow(){
+        if(calculated)return max_flow;
+        calculated=true;
+        max_flow=0;
+        for(T scale=scaling?1LL<<(63-__builtin_clzll(U)):1LL;scale>0;scale>>=1){
+            while(bfs(scale)){
+                ptr.assign(n,0);
+                while(true){
+                    T f=dfs(s,t,INF);
+                    if(f==0)break;
+                    max_flow+=f;
+                }
+            }
+        }
+        return max_flow;
+    }
+    pair<T,vector<int>> cut(){
+        flow();
+        vector<int> res(n);
+        for(int i=0;i<n;i++)res[i]=(lv[i]==-1);
+        return {max_flow,res};
     }
 };
-
--- content/number-theory/euclid.h
+++ content/number-theory/euclid.h
@@ -5,6 +5,8 @@
  * Description: Finds two integers $x$ and $y$, such that $ax+by=\gcd(a,b)$. If
  * you just need gcd, use the built in \texttt{\_\_gcd} instead.
  * If $a$ and $b$ are coprime, then $x$ is the inverse of $a \pmod{b}$.
+ * $x = x_0 + k * (b / g)$
+ * $y = y_0 - k * (a / g)$
  */
 #pragma once
 
