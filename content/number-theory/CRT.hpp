56390f55a9075d033603aa4e076ff31b3cb17bb7
ttamx <teetat.tata@gmail.com>
2024-11-27 19:02:47 +0700

    add description for CRT

--- content/number-theory/CRT.hpp
+++ content/number-theory/CRT.hpp
@@ -7,6 +7,8 @@
  * \texttt{crt(a, m, b, n)} computes $x$ such that $x\equiv a \pmod m$, $x\equiv b \pmod n$.
  * If $|a| < m$ and $|b| < n$, $x$ will obey $0 \le x < \text{lcm}(m, n)$.
  * Assumes $mn < 2^{62}$.
+ * If x0 and y0 is one of the solutions of ax + by = g, then the general solution is
+ * x = x0 + k * (b / g) and y = y0 - k * (a / g).
  * Time: $\log(n)$
  * Status: Works
  */
