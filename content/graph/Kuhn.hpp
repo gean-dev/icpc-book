ce08f656852ffc0ec0c2751cf52dd4d5b1514023
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-12-19 02:00:26 +0700

    fix: kuhn

--- content/graph/Kuhn.hpp
+++ content/graph/Kuhn.hpp
@@ -9,14 +9,14 @@
  */
 
 vi adj[1010], match(1010, -1);
-bitset<1010> visited;
-bool kuhn(int u) {
+vector<bool> visited(1010, false);
+bool try_match(int u) {
   if(visited[u]) {
     return false;
   } 
   visited[u] = true;
   for(auto x: adj[u]) {
-    if(match[x] == -1 || kuhn(match[x])) {
+    if(match[x] == -1 || try_match(match[x])) {
       match[x] = u;
       return true;
     }
@@ -24,3 +24,14 @@ bool kuhn(int u) {
   return false;
 }
 
+int max_matching() {
+  for(int u=0; u<1010; ++u) {
+    visited = vector<bool> (1010, false);
+    try_match(u);
+  }
+  int cnt = 0;
+  for(int u=0; u<1010; ++u) {
+    cnt += (match[u] != -1);
+  }
+  return cnt;
+}
